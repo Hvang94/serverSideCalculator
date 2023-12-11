9c53c51cbf2f2c44cb495ddfb24209e5d55a573e
Hmoovzong Vang <hmoovzongvang@gmail.com>
2023-12-10 20:58:21 -0600

    Server is running. Able to send get a post requests to the server using postman.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,57 @@
-console.log('client.js is sourced!');
+console.log("client.js is sourced!");
+function onReady() {
+  renderMath();
+}
+
+let operator;
+function setOperator(symbol, event) {
+  event.preventDefault();
+  operator = symbol;
+  console.log(num1, num2, operator);
+}
+
+function calculateHandler(event) {
+  event.preventDefault();
+
+  let num1 = parseFloat(document.getElementById("firstInput").value);
+  let num2 = parseFloat(document.getElementById("secondInput").value);
+
+  axios({
+    method: "POST",
+    url: "/calculator",
+    data: {
+      num1: num1,
+      num2: num2,
+      operator: operator,
+    },
+  })
+    .then((response) => {
+      console.log("successfully added", response);
+    })
+    .catch((error) => {
+      console.log("server error", error);
+    });
+}
+
+function renderMath() {
+  axios({
+    method: "GET",
+    url: "/calculator",
+  })
+    .then((response) => {
+      console.log("response from get", response.data);
+      let recentResults = document.getElementById("recentResult");
+      let resultHistory = document.getElementById("resultHistory");
+      let incomingArr = response.data;
+      let results = result;
+
+      recentResults.innerHTML = "";
+      resultHistory.innerHTML = ""
+      recentResults += results;
+      resultHistory =+ `<div> ${numOne} ${operator} ${numTwo} = ${result}</div>`
+    })
+    .catch((error) => {
+      console.log("server error", error);
+    });
+}
+onReady();
--- server/server.js
+++ server/server.js
@@ -1,28 +1,60 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Global variable that will contain all of the
 // calculation objects:
-let calculations = []
-
-
+let serverCalculate = [];
+let result = 0;
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
-
+app.get("/calculator", (req, res) => {
+  console.log("inside /get request", serverCalculate);
+  res.send(serverCalculate);
+});
 // POST /calculations
+app.post("/calculator", (req, res) => {
+  console.log("inside /post request", req.body);
+  let num1 = req.body.num1;
+  let num2 = req.body.num2;
+  let operator = req.body.operator;
 
-
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+  }
+  serverCalculate.push({
+    numOne: num1,
+    numTwo: num2,
+    operator: operator,
+    result: result,
+  });
+  res.sendStatus(200);
+});
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 
 // Makes it so you don't have to kill the server
 // on 5000 in order to run the tests:
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   PORT = 5001;
 }
 
@@ -30,7 +62,7 @@ if (process.env.NODE_ENV === 'test') {
 // This is weird. We have to do it for testing reasons. There
 // is absolutely no need for you to reason about this.
 const server = app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
+  console.log("server running on: ", PORT);
 });
 
 // server.setTimeout(500)
@@ -39,10 +71,10 @@ const server = app.listen(PORT, () => {
 // absolutely no need for you to reason about this.
 app.closeServer = () => {
   server.close();
-}
+};
 
 app.setCalculations = (calculationsToSet) => {
   calculations = calculationsToSet;
-}
+};
 
 module.exports = app;
